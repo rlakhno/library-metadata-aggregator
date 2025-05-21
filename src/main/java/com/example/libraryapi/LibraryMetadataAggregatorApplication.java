90815bef23bd521d7042876ff9f3df823148b16a
Rouslan <rouslanlakhno@gmail.com>
2025-05-21 17:52:32 -0400

    Add model/User.java and repository/UserRepository.java

--- src/main/java/com/example/libraryapi/LibraryMetadataAggregatorApplication.java
+++ src/main/java/com/example/libraryapi/LibraryMetadataAggregatorApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LibraryMetadataAggregatorApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LibraryMetadataAggregatorApplication.class, args);
 	}
 
---
+++ src/main/java/com/example/libraryapi/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.libraryapi.repository;
+
+import com.example.libraryapi.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByName(String username);
+    boolean existsByName(String username);
+    boolean existsByEmail(String email);
+
+}
