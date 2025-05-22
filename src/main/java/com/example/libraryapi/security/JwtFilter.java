6a63b28e679e9904025091ffced89c6897ae3803
Rouslan <rouslanlakhno@gmail.com>
2025-05-22 18:53:54 -0400

    Add security/JwtFilter.java

--- src/main/java/com/example/libraryapi/security/JwtFilter.java
+++ src/main/java/com/example/libraryapi/security/JwtFilter.java
@@ -1,8 +1,18 @@
 package com.example.libraryapi.security;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
@@ -14,4 +24,48 @@ public class JwtFilter extends OncePerRequestFilter {
     private CustomUserDetailsService userDetailsService;
 
 
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+        String username = null;
+        String jwtToken = null;
+
+        // Check Authorization header
+        if(authHeader != null && authHeader.startsWith("Bearer ")) {
+            // Remove "Bearer "
+            jwtToken = authHeader.substring(7);
+            try {
+                // get username (subject) from token
+                username = jwtUtil.extractUsername(jwtToken);
+            } catch (Exception e) {
+                System.out.println("Invalid JWT Token: " + e.getMessage());
+            }
+        }
+
+        // If token is valid and user is not yet authenticated - Loads user
+        // from DB using username
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+            // Validates the JWT against that user
+            if(jwtUtil.validateToken(jwtToken,userDetails)) {
+                // Creates a Spring Security Authentication token
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+               // Stores it in the security context, marking the user as authenticated
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        // Pass the request to the next filter
+        filterChain.doFilter(request,response);
+
+
+    }
 }
